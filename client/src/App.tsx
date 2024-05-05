b050368e701b00cece097e29fd137829cd3edb09
ViniciosLugli <vinicioslugli@gmail.com>
2024-05-05 15:42:16 -0300

    refactor: update dependencies and add react-hot-toast for notifications

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,41 +1,42 @@
-import GlobalStyles from "./styles/GlobalStyles"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import GlobalStyles from './styles/GlobalStyles';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 // Login and Register pages
-import Welcome from "./pages/Welcome";
-import Register from "./pages/Register";
+import Welcome from './pages/Welcome';
+import Register from './pages/Register';
 
 // Retailer pages
-import Home from "./pages/Home";
-import Products from "./pages/Products";
-import Score from "./pages/Score";
-import Orders from "./pages/Orders";
-import Customers from "./pages/Customers";
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Score from './pages/Score';
+import Orders from './pages/Orders';
+import Customers from './pages/Customers';
 
 // Customer pages
-import CustomerHome from "./pages/CustomerHome";
-import CustomerBuy from "./pages/CustomerBuy";
-import CustomerScore from "./pages/CustomerScore";
+import CustomerHome from './pages/CustomerHome';
+import CustomerBuy from './pages/CustomerBuy';
+import CustomerScore from './pages/CustomerScore';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
-
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/retailerHome" element={<Home />} />
-        <Route path="/retailerProducts" element={<Products code={0} ipfsHash={""} name={""} tags={[]} price={0} score={0} />} />
-        <Route path="/retailerScore" element={<Score />} />
-        <Route path="/retailerOrders" element={<Orders />} />
-        <Route path="/retailerCustomers" element={<Customers />} />
-        <Route path="/customerHome" element={<CustomerHome />} />
-        <Route path="/customerBuy" element={<CustomerBuy />} />
-        <Route path="/customerScore" element={<CustomerScore />} />
-      </Routes>
-      <GlobalStyles />
-    </>
-  )
+	return (
+		<>
+			<Toaster />
+			<Routes>
+				<Route path="/" element={<Welcome />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/retailerHome" element={<Home />} />
+				<Route path="/retailerProducts" element={<Products code={0} ipfsHash={''} name={''} tags={[]} price={0} score={0} />} />
+				<Route path="/retailerScore" element={<Score />} />
+				<Route path="/retailerOrders" element={<Orders />} />
+				<Route path="/retailerCustomers" element={<Customers />} />
+				<Route path="/customerHome" element={<CustomerHome />} />
+				<Route path="/customerBuy" element={<CustomerBuy />} />
+				<Route path="/customerScore" element={<CustomerScore />} />
+			</Routes>
+			<GlobalStyles />
+		</>
+	);
 }
 
 export default App;
