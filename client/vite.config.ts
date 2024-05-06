3f02610b247f9e5e9b46f47121eaf2ef6ce99b54
ViniciosLugli <vinicioslugli@gmail.com>
2024-05-05 23:29:55 -0300

    chore: Update Vite configuration to use Vercel environment variable

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,10 +5,6 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
 	plugins: [react()],
 	define: {
-		'process.env': process.env,
-		VITE_DEPLOY_IPFS_FOLDER_URL: process.env.VITE_DEPLOY_IPFS_FOLDER_URL,
-		VITE_LUMX_BEARER: process.env.VITE_LUMX_BEARER,
-		VITE_WEB3_PROVIDER: process.env.VITE_WEB3_PROVIDER,
-		VITE_WEB3_WSS_PROVIDER: process.env.VITE_WEB3_WSS_PROVIDER,
+		__APP_ENV__: process.env.VITE_VERCEL_ENV,
 	},
 });
