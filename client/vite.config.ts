a28afd928651073c71515cf4ca62b41a30901aa5
ViniciosLugli <vinicioslugli@gmail.com>
2024-05-05 23:25:49 -0300

    chore: update Vite configuration and environment variables

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	define: {
+		'process.env': process.env,
+		VITE_DEPLOY_IPFS_FOLDER_URL: process.env.VITE_DEPLOY_IPFS_FOLDER_URL,
+		VITE_LUMX_BEARER: process.env.VITE_LUMX_BEARER,
+		VITE_WEB3_PROVIDER: process.env.VITE_WEB3_PROVIDER,
+		VITE_WEB3_WSS_PROVIDER: process.env.VITE_WEB3_WSS_PROVIDER,
+	},
+});
